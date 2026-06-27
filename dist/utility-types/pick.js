86cae9697abc94758c03ceadc98a33f9de21c7bf
SamuelRodriguezC <samuelrc250@gmail.com>
2026-06-26 19:16:13 -0500

    compilación de pick

--- dist/utility-types/pick.js
+++ dist/utility-types/pick.js
@@ -1,3 +1,5 @@
 "use strict";
+// permite crear un tipo sincronizado pero con unas propiedades 
+// especificas seleccionadas
 Object.defineProperty(exports, "__esModule", { value: true });
 //# sourceMappingURL=pick.js.map
